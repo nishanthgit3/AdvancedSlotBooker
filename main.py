d69a736ac2b0329d6d6995f37fc56a1903f35729
Nishanth <nishanthscontact@gmail.com>
2026-01-29 15:53:17 +0530

    Finish showCourseMenu

--- main.py
+++ main.py
@@ -12,18 +12,32 @@ root.geometry("925x800")
 # Functions
 
 # Logics
+allCourses = ["No courses available"]
 def getCourse():
     url = 'https://ps.bitsathy.ac.in/api/ps_v2/my-course?tab=personalizedSkills'
     try:
         r = requests.get(url, headers=headers)
         data = r.json()
+        global allCourses
+        allCourses = []
+        for i in data:
+            allCourses.append(i["name"])
+        showCourseMenu()
     except:
         print("Please enter a cookie before requesting for the courses")
 
 # GUI
 def cookieButtonClick():
     cookie = cookieEntry.get()
+    global headers 
     headers = {'Host':'ps.bitsathy.ac.in', 'Cookie': 'PS='+cookie}
+    getCourse()
+
+def showCourseMenu():
+    course = StringVar()
+    course.set("Select a course")
+    coursesMenu = OptionMenu(root, course, *allCourses)
+    coursesMenu.grid(row=1, column=1)
 
 # Creating
 # Row 1
@@ -32,12 +46,15 @@ cookieEntry = Entry(root, width=50)
 cookieButton = Button(root, text="✓", command=cookieButtonClick)
 
 # Row 2
-# coursesMenu = OptionMenu(root, course, "")
+# course = StringVar()
+# course.set("Select a course")
+# coursesMenu = OptionMenu(root, course, *allCourses)
 
 # Showing/Grids
 cookieText.grid(row=0, column=0)
 cookieEntry.grid(row=0, column=1)
 cookieButton.grid(row=0, column=2)
+# coursesMenu.grid(row=1, column=1)
 
 
 
